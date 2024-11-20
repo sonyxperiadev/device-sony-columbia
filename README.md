6ed5730ce926d17f53eb8760b2ba399ac5195c2f
Alin Jerpelea <alin.jerpelea@sony.com>
2024-11-20 13:19:37 +0100

    initial config for columbia platform
    Signed-off-by: Alin Jerpelea <alin.jerpelea@sony.com>

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# device-sony-columbia
+columbia platform
+============
+Copyright (C) Sony Mobile Communications 2024
+---------------------------------------------
+
+Android device configuration for the columbia platform (**SM6450**).
+
+### Supported devices
+
+| Device | Codename |
+|-|:-:|
+| Xperia 10 VI | [pdx246](https://github.com/sonyxperiadev/device-sony-pdx246) |
+
+### Build instructions
+
+https://developer.sony.com/develop/open-devices/guides/aosp-build-instructions/
---
+++ platform.mk
@@ -0,0 +1,482 @@
+# Copyright (C) 2014 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Platform path
+PLATFORM_COMMON_PATH := device/sony/columbia
+
+PARROT := sm6450
+
+SOMC_PLATFORM := columbia
+SOMC_KERNEL_VERSION := 5.10
+
+PRODUCT_PLATFORM_SOD := true
+
+TARGET_BOARD_PLATFORM := $(PARROT)
+
+SONY_ROOT := $(PLATFORM_COMMON_PATH)/rootdir
+
+# Overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(PLATFORM_COMMON_PATH)/overlay
+
+# RIL
+TARGET_PER_MGR_ENABLED := true
+
+TARGET_PD_SERVICE_ENABLED := true
+
+# Wi-Fi definitions for Qualcomm solution
+WIFI_DRIVER_BUILT := qca_cld3
+WIFI_DRIVER_DEFAULT := qca_cld3
+BOARD_HAS_QCOM_WLAN := true
+BOARD_HOSTAPD_DRIVER := NL80211
+BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
+BOARD_WLAN_DEVICE := qcwcn
+BOARD_WLAN_CHIP := wcn6740
+BOARD_WPA_SUPPLICANT_DRIVER := NL80211
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_qcwcn
+HOSTAPD_VERSION := VER_0_8_X
+WIFI_DRIVER_FW_PATH_AP  := "ap"
+WIFI_DRIVER_FW_PATH_P2P := "p2p"
+WIFI_DRIVER_FW_PATH_STA := "sta"
+WPA_SUPPLICANT_VERSION := VER_0_8_X
+TARGET_USES_ICNSS_QMI := true
+WIFI_DRIVER_STATE_CTRL_PARAM := "/dev/wlan"
+WIFI_DRIVER_STATE_OFF := "OFF"
+WIFI_DRIVER_STATE_ON := "ON"
+
+# Add BOARD_WLAN_CHIP to soong_config
+$(call soong_config_set,qcom_wifi,board_wlan_chip,wcn6740)
+
+# BT definitions for Qualcomm solution
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_QCOM := true
+TARGET_USE_QTI_BT_STACK := true
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(PLATFORM_COMMON_PATH)/bluetooth
+WCNSS_FILTER_USES_SIBS := true
+
+# NFC
+NXP_CHIP_FW_TYPE := PN557
+
+# Audio
+AUDIO_FEATURE_ENABLED_AHAL_EXT := false
+AUDIO_FEATURE_ENABLED_CONCURRENT_CAPTURE := true
+AUDIO_FEATURE_ENABLED_DTS_EAGLE := false
+AUDIO_FEATURE_ENABLED_DLKM := false
+AUDIO_FEATURE_ENABLED_DS2_DOLBY_DAP := false
+AUDIO_FEATURE_ENABLED_DYNAMIC_LOG := false
+AUDIO_FEATURE_ENABLED_GEF_SUPPORT := true
+AUDIO_FEATURE_ENABLED_GKI := true
+AUDIO_FEATURE_ENABLED_HW_ACCELERATED_EFFECTS := false
+AUDIO_FEATURE_ENABLED_INSTANCE_ID := true
+AUDIO_FEATURE_ENABLED_PROXY_DEVICE := true
+AUDIO_FEATURE_ENABLED_SSR := true
+BOARD_SUPPORTS_OPENSOURCE_STHAL := true
+BOARD_SUPPORTS_QAHW := false
+BOARD_SUPPORTS_SOUND_TRIGGER := true
+BOARD_USES_ALSA_AUDIO := true
+TARGET_USES_QCOM_MM_AUDIO := true
+
+# Dynamic Partitions: Enable DP
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Display
+TARGET_HAS_HDR_DISPLAY := true
+TARGET_RECOVERY_PIXEL_FORMAT := BGRA_8888
+TARGET_USES_DRM_PP := true
+NUM_FRAMEBUFFER_SURFACE_BUFFERS := 2
+
+# Lights HAL: Backlight
+TARGET_USES_SDE := true
+
+# A/B support
+AB_OTA_UPDATER := true
+PRODUCT_SHIPPING_API_LEVEL := 27
+
+# A/B OTA dexopt package
+PRODUCT_PACKAGES += \
+    otapreopt_script
+
+# A/B OTA dexopt update_engine hookup
+AB_OTA_POSTINSTALL_CONFIG += \
+    RUN_POSTINSTALL_system=true \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+    FILESYSTEM_TYPE_system=ext4 \
+    POSTINSTALL_OPTIONAL_system=true
+
+# A/B related packages
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_client \
+    update_engine_sideload \
+    update_verifier
+
+AB_OTA_PARTITIONS += \
+    boot \
+    dtbo \
+    product \
+    system \
+    system_ext \
+    vbmeta \
+    vbmeta_system \
+    vendor \
+    vendor_boot
+
+# Dynamic Partitions: build fastbootd
+PRODUCT_PACKAGES += \
+    fastbootd
+
+# Treble
+# Include vndk/vndk-sp/ll-ndk modules
+PRODUCT_PACKAGES += \
+    vndk_package
+
+# Device Specific Permissions
+PRODUCT_COPY_FILES += \
+     frameworks/native/data/etc/android.hardware.sensor.gyroscope.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.gyroscope.xml \
+     frameworks/native/data/etc/android.hardware.sensor.barometer.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.barometer.xml \
+     frameworks/native/data/etc/android.hardware.sensor.stepcounter.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepcounter.xml \
+     frameworks/native/data/etc/android.hardware.sensor.stepdetector.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.sensor.stepdetector.xml
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/vendor/etc/audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/audio_policy_configuration.xml \
+    $(SONY_ROOT)/vendor/etc/bluetooth_qti_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/bluetooth_qti_audio_policy_configuration.xml \
+    $(SONY_ROOT)/vendor/etc/bluetooth_qti_hearing_aid_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/bluetooth_qti_hearing_aid_audio_policy_configuration.xml\
+    $(SONY_ROOT)/vendor/etc/media_codecs_vendor_audio.xml:$(TARGET_COPY_OUT_VENDOR)/media_codecs_vendor_audio.xml \
+    $(SONY_ROOT)/vendor/etc/mixer_paths_parrot_qrd_sku1.xml:$(TARGET_COPY_OUT_VENDOR)/mixer_paths_parrot_qrd_sku1.xml
+
+# Media
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/vendor/etc/media_codecs_parrot_v0.xml:$(TARGET_COPY_OUT_VENDOR)/media_codecs_parrot_v0.xml \
+    $(SONY_ROOT)/vendor/etc/media_codecs_parrot_v1.xml:$(TARGET_COPY_OUT_VENDOR)/media_codecs_parrot_v1.xml \
+    $(SONY_ROOT)/vendor/etc/media_codecs_parrot_v2.xml:$(TARGET_COPY_OUT_VENDOR)/media_codecs_parrot_v2.xml \
+    $(SONY_ROOT)/vendor/etc/media_codecs_performance_parrot_v0.xml:$(TARGET_COPY_OUT_VENDOR)/media_codecs_performance_parrot_v0.xml \
+    $(SONY_ROOT)/vendor/etc/media_codecs_performance_parrot_v1.xml:$(TARGET_COPY_OUT_VENDOR)/media_codecs_performance_parrot_v1.xml \
+    $(SONY_ROOT)/vendor/etc/media_codecs_performance_parrot_v2.xml:$(TARGET_COPY_OUT_VENDOR)/media_codecs_performance_parrot_v2.xml \
+    $(SONY_ROOT)/vendor/etc/media_parrot_v0/video_system_specs.json:$(TARGET_COPY_OUT_VENDOR)/media_parrot_v0/video_system_specs.json \
+    $(SONY_ROOT)/vendor/etc/media_parrot_v1/video_system_specs.json:$(TARGET_COPY_OUT_VENDOR)/media_parrot_v1/video_system_specs.json \
+    $(SONY_ROOT)/vendor/etc/media_parrot_v2/video_system_specs.json:$(TARGET_COPY_OUT_VENDOR)/media_parrot_v2/video_system_specs.json \
+    $(SONY_ROOT)/vendor/etc/media_profiles_parrot_v0.xml:$(TARGET_COPY_OUT_VENDOR)/media_profiles_parrot_v0.xml \
+    $(SONY_ROOT)/vendor/etc/media_profiles_parrot_v1.xml:$(TARGET_COPY_OUT_VENDOR)/media_profiles_parrot_v1.xml \
+    $(SONY_ROOT)/vendor/etc/media_profiles_parrot_v2.xml:$(TARGET_COPY_OUT_VENDOR)/media_profiles_parrot_v2.xml
+
+# Qualcom WiFi Overlay
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/vendor/etc/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(SONY_ROOT)/vendor/etc/wifi/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf
+
+# Qualcom WiFi Configuration
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/vendor/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/firmware/wlan/qca_cld/WCNSS_qcom_cfg.ini
+
+# NFC Configuration
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/vendor/etc/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
+
+# FPC Gestures
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/vendor/usr/idc/uinput-fpc.idc:$(TARGET_COPY_OUT_VENDOR)/usr/idc/uinput-fpc.idc \
+    $(SONY_ROOT)/vendor/usr/keylayout/uinput-fpc.kl:$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/uinput-fpc.kl
+
+# MSM IRQ Balancer configuration file
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/vendor/etc/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
+
+# CAMX config
+PRODUCT_COPY_FILES += \
+    $(SONY_ROOT)/vendor/etc/camera/camxoverridesettings.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camxoverridesettings.txt
+
+# Platform specific init
+PRODUCT_PACKAGES += \
+    tad.rc \
+    init.columbia \
+    init.columbia.pwr \
+    ueventd
+
+# Audio init
+PRODUCT_PACKAGES += \
+    audiopd.rc
+
+# Audio
+PRODUCT_PACKAGES += \
+    sound_trigger.primary.sm6450 \
+    audio.primary.sm6450
+
+# Camera
+PRODUCT_PACKAGES += \
+
+# GFX
+PRODUCT_PACKAGES += \
+    copybit.sm6450 \
+    gralloc.sm6450 \
+    hwcomposer.sm6450 \
+    memtrack.default
+
+# HVDCP init
+PRODUCT_PACKAGES += \
+    hvdcp_opti.rc
+
+# Keymaster 4 passthrough service init file
+# (executable is on odm)
+PRODUCT_PACKAGES += \
+    android.hardware.keymaster@4.1-service-qti.rc \
+    android.hardware.keymaster@4.1.vendor \
+    vendor.qti.hardware.qseecom@1.0-service.rc
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.keymaster.version=v4.1 \
+    ro.crypto.dm_default_key.options_format.version=2 \
+    ro.crypto.volume.metadata.method=dm-default-key
+
+DEVICE_MANIFEST_FILE += \
+    $(PLATFORM_COMMON_PATH)/vintf/android.hw.keymaster_v4.1.xml \
+    $(PLATFORM_COMMON_PATH)/vintf/vendor.qti.hardware.qseecom_v1.0.xml
+
+# GPS
+PRODUCT_PACKAGES += \
+    gps.sm6450
+
+# Sensors init
+PRODUCT_PACKAGES += \
+    sscrpcd.rc \
+    sdsp-sensorspdr.rc
+
+# Sensors
+# hardware.ssc.so links against display mappers, of which
+# the interface libraries are explicitly included here:
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@2.1-service.multihal \
+    vendor.qti.hardware.display.mapper@1.1.vendor \
+    vendor.qti.hardware.display.mapper@3.0.vendor
+
+# Sensors
+PRODUCT_PACKAGES += \
+    sns_reg_config \
+    hals.conf
+
+# SSC Common config
+PRODUCT_PACKAGES += \
+    ak991x_dri_0.json \
+    bma2x2_0.json \
+    bme680_0.json \
+    bmg160_0.json \
+    bmp285_0.json \
+    bmp380_0.json \
+    bu52053nvx_0.json \
+    cm3526_0.json \
+    default_sensors.json \
+    dps368_0.json \
+    lsm6dsm_0_16g.json \
+    lsm6dsm_0.json \
+    lsm6dso_0_16g.json \
+    lsm6dso_0.json \
+    shtw2_0.json \
+    sns_amd.json \
+    sns_amd_sw_disabled.json \
+    sns_amd_sw_enabled.json \
+    sns_aont.json \
+    sns_basic_gestures.json \
+    sns_bring_to_ear.json \
+    sns_ccd.json \
+    sns_cm.json \
+    sns_dae.json \
+    sns_device_orient.json \
+    sns_diag_filter.json \
+    sns_distance_bound.json \
+    sns_dpc.json \
+    sns_facing.json \
+    sns_fmv.json \
+    sns_geomag_rv.json \
+    sns_gyro_cal.json \
+    sns_mag_cal.json \
+    sns_multishake.json \
+    sns_pedometer.json \
+    sns_rmd.json \
+    sns_rotv.json \
+    sns_smd.json \
+    sns_tilt.json \
+    sns_tilt_sw_disabled.json \
+    sns_tilt_sw_enabled.json \
+    sns_tilt_to_wake.json \
+    tmd2725.json \
+    tmd3725.json \
+    tmx4903.json
+
+# Platform SSC Sensors
+PRODUCT_PACKAGES += \
+
+# Other columbia-specific sensors
+PRODUCT_PACKAGES += \
+
+# Platform-specific sensor overlays
+PRODUCT_COPY_FILES += \
+
+# CAMERA
+TARGET_USES_64BIT_CAMERA := true
+
+# Look for camera.qcom.so instead of camera.$(BOARD_TARGET_PLATFORM).so
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.hardware.camera=qcom
+
+# QCOM Bluetooth
+PRODUCT_PACKAGES += \
+    android.hardware.bluetooth@1.0-impl-qti \
+    android.hardware.bluetooth@1.0-service-qti
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.qcom.bluetooth.soc=hastings
+
+# Legacy BT property (will be removed in S)
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.qcom.bluetooth.soc=hastings
+
+# Audio - QCOM HAL
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.audio_hal.period_size=192 \
+    persist.vendor.audio.ambisonic.capture=false \
+    persist.vendor.audio.ambisonic.auto.profile=false \
+    persist.vendor.audio.apptype.multirec.enabled=false \
+    ro.vendor.audio.sdk.fluencetype=none \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.tmic.enabled=false \
+    persist.vendor.audio.spv3.enable=true \
+    persist.vendor.audio.avs.afe_api_version=2 \
+    vendor.audio.tunnel.encode=false \
+    persist.vendor.audio.ras.enabled=false \
+    vendor.audio.offload.buffer.size.kb=32 \
+    vendor.voice.path.for.pcm.voip=true \
+    vendor.audio.dolby.ds2.enabled=false \
+    vendor.audio.offload.multiple.enabled=false \
+    vendor.audio.offload.passthrough=false \
+    ro.vendor.audio.sdk.ssr=false \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.safx.pbe.enabled=false \
+    ro.bluetooth.a2dp_offload.supported=true \
+    persist.bluetooth.a2dp_offload.disabled=false \
+    vendor.audio.use.sw.alac.decoder=true \
+    vendor.audio.use.sw.ape.decoder=true \
+    vendor.audio_hal.in_period_size=144 \
+    vendor.audio_hal.period_multiplier=3 \
+    vendor.audio.volume.headset.gain.depcal=true \
+    persist.vendor.audio.fluence.voicecomm=true \
+    vendor.audio.hal.output.suspend.supported=true \
+    persist.vendor.audio.voicecall.speaker.stereo=true \
+    persist.vendor.bt.aac_frm_ctl.enabled=true \
+    persist.vendor.bt.aac_vbr_frm_ctl.enabled=true
+
+# Audio dynamic feature flags
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.audio.feature.a2dp_offload.enable=true \
+    vendor.audio.feature.afe_proxy.enable=true \
+    vendor.audio.feature.anc_headset.enable=false \
+    vendor.audio.feature.battery_listener.enable=true \
+    vendor.audio.feature.compr_cap.enable=false \
+    vendor.audio.feature.compress_in.enable=true \
+    vendor.audio.feature.compress_meta_data.enable=true \
+    vendor.audio.feature.compr_voip.enable=false \
+    vendor.audio.feature.concurrent_capture.enable=true \
+    vendor.audio.feature.custom_stereo.enable=true \
+    vendor.audio.feature.display_port.enable=true \
+    vendor.audio.feature.dsm_feedback.enable=false \
+    vendor.audio.feature.dynamic_ecns.enable=true \
+    vendor.audio.feature.ext_hw_plugin.enable=false \
+    vendor.audio.feature.external_dsp.enable=false \
+    vendor.audio.feature.external_speaker.enable=false \
+    vendor.audio.feature.external_speaker_tfa.enable=false \
+    vendor.audio.feature.fluence.enable=true \
+    vendor.audio.feature.fm.enable=true \
+    vendor.audio.feature.hdmi_edid.enable=true \
+    vendor.audio.feature.hdmi_passthrough.enable=true \
+    vendor.audio.feature.hfp.enable=true \
+    vendor.audio.feature.hifi_audio.enable=false \
+    vendor.audio.feature.hwdep_cal.enable=false \
+    vendor.audio.feature.incall_music.enable=true \
+    vendor.audio.feature.multi_voice_session.enable=true \
+    vendor.audio.feature.keep_alive.enable=true \
+    vendor.audio.feature.kpi_optimize.enable=true \
+    vendor.audio.feature.maxx_audio.enable=false \
+    vendor.audio.feature.ras.enable=true \
+    vendor.audio.feature.record_play_concurency.enable=false \
+    vendor.audio.feature.src_trkn.enable=true \
+    vendor.audio.feature.spkr_prot.enable=true \
+    vendor.audio.feature.ssrec.enable=true \
+    vendor.audio.feature.usb_offload.enable=true \
+    vendor.audio.feature.usb_offload_burst_mode.enable=true \
+    vendor.audio.feature.usb_offload_sidetone_volume.enable=false \
+    vendor.audio.feature.deepbuffer_as_primary.enable=false \
+    vendor.audio.feature.vbat.enable=true \
+    vendor.audio.feature.wsa.enable=false \
+    vendor.audio.feature.audiozoom.enable=false \
+    vendor.audio.feature.snd_mon.enable=true
+
+# Audio - Sony specific
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.audio.enable.cirrus.speaker=true
+
+# USB controller setup
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.boot.usb.dwc3=4e00000.ssusb \
+    sys.usb.controller=4e00000.dwc3 \
+    sys.usb.rndis.func.name=gsi
+
+#WiFi MAC address path
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.wifi.addr_path=/data/vendor/wifi/wlan_mac.bin
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.display.comp_mask=0 \
+    vendor.display.disable_excl_rect=0 \
+    vendor.display.disable_excl_rect_partial_fb=1 \
+    vendor.display.disable_hw_recovery_dump=1 \
+    vendor.display.disable_layer_stitch=0 \
+    vendor.display.disable_rotator_ubwc=1 \
+    vendor.display.disable_scaler=0 \
+    vendor.gralloc.disable_ubwc=0 \
+    vendor.display.enable_posted_start_dyn=1 \
+    vendor.display.enable_optimize_refresh=1 \
+    vendor.display.camera_noc_efficiency_factor=0.70 \
+    vendor.display.normal_noc_efficiency_factor=0.85 \
+    vendor.display.secure_preview_buffer_format=420_sp \
+    vendor.display.use_smooth_motion=1 \
+    vendor.gralloc.secure_preview_buffer_format=420_sp \
+    debug.sf.enable_advanced_sf_phase_offset=1 \
+    debug.sf.high_fps_late_sf_phase_offset_ns=-5000000 \
+    debug.sf.high_fps_early_phase_offset_ns=-5000000 \
+    debug.sf.high_fps_early_gl_phase_offset_ns=-5000000
+
+# Display - HDR/WCG
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.display.dataspace_saturation_matrix=1.0,0.0,0.0,0.0,1.0,0.0,0.0,0.0,1.0 \
+    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.protected_contents=true \
+    ro.surface_flinger.set_touch_timer_ms=200 \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696
+
+# Gatekeeper
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.gatekeeper.disable_spu=true
+
+# Camera
+PRODUCT_PROPERTY_OVERRIDES += \
+    camera.disable_zsl_mode=1
+
+$(call inherit-product, device/sony/common/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
