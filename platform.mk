d551ff02a6f9972857ec6bfec733f9cd8b75fb9a
Pavel Dubrova <pashadubrova@gmail.com>
2026-07-01 08:23:56 +0300

    overlay: Convert build-time overlays to RRO
    Convert all build-time overlays to Runtime Resource
    Overlays (RROs).
    
    adb shell cmd overlay list | grep columbia
    [x] net.sony.opendevices.framework.overlay.columbia
    [x] net.sony.opendevices.settings.overlay.columbia
    
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- platform.mk
+++ platform.mk
@@ -26,10 +26,6 @@ TARGET_BOARD_PLATFORM := $(PARROT)
 
 SONY_ROOT := $(PLATFORM_COMMON_PATH)/rootdir
 
-# Overlay
-DEVICE_PACKAGE_OVERLAYS += \
-    $(PLATFORM_COMMON_PATH)/overlay
-
 # Build recovery image
 PRODUCT_BUILD_RECOVERY_IMAGE := true
 
@@ -141,6 +137,11 @@ PRODUCT_COPY_FILES += \
 # CAMX config
 PRODUCT_COPY_FILES += \
     $(SONY_ROOT)/vendor/etc/camera/camxoverridesettings.txt:$(TARGET_COPY_OUT_VENDOR)/etc/camera/camxoverridesettings.txt
+
+# Platform Runtime Resource Overlays
+PRODUCT_PACKAGES += \
+    FrameworkOverlayColumbia \
+    SettingsOverlayColumbia
 
 # Platform specific init
 PRODUCT_PACKAGES += \
